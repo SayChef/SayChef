a7a56c69bab7d9a44b7f85060660c723de68451a
David Keller <dev@davidkeller.me>
2016-01-23 02:18:19 -0500

    Rename Dave to David Keller

--- Talk2Cook/MasterViewController.swift
+++ Talk2Cook/MasterViewController.swift
@@ -2,7 +2,7 @@
 //  MasterViewController.swift
 //  Talk2Cook
 //
-//  Created by Dave on 22.01.16.
+//  Created by David Keller on 22.01.16.
 //  Copyright © 2016 PennApps XIII. All rights reserved.
 //
 
