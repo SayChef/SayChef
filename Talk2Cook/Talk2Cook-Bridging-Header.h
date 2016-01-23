36d1d9f05857e24e3ef787dc7a0a64ad98b8ba86
sjono <jono.sanders@gmail.com>
2016-01-23 01:42:50 -0500

    HoundSDK Updates
    Simple setup for the HoundSDK

---
+++ Talk2Cook/Talk2Cook-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Talk2Cook-Bridging-Header.h
+//  Talk2Cook
+//
+//  Created by Jonathan Sanders on 1/23/16.
+//  Copyright © 2016 PennApps XIII. All rights reserved.
+//
+
+#ifndef Talk2Cook_Bridging_Header_h
+#define Talk2Cook_Bridging_Header_h
+
+#import <HoundSDK/HoundSDK.h>
+
+
+#endif /* Talk2Cook_Bridging_Header_h */
