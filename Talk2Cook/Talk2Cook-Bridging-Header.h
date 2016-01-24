1222bd2ebcd30baa1a0eadbdb78a112dceb1c0ac
David Keller <dev@davidkeller.me>
2016-01-24 01:12:05 -0500

    Add HoundSDK

---
+++ Talk2Cook/Talk2Cook-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Talk2Cook-Bridging-Header.h
+//  Talk2Cook
+//
+//  Created by David Keller on 24.01.16.
+//  Copyright © 2016 PennApps XIII. All rights reserved.
+//
+
+#ifndef Talk2Cook_Bridging_Header_h
+#define Talk2Cook_Bridging_Header_h
+
+#import <HoundSDK/HoundSDK.h>
+
+#endif /* Talk2Cook_Bridging_Header_h */
